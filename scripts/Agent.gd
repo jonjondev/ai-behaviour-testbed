697dc97bc04e1e124bc64f489252019bebdb8eac
Jonathan Moallem <jdmoallem@gmail.com>
2020-05-09 01:21:10 +1000

    Updated agent class to work with behaviour trees

--- scripts/Agent.gd
+++ scripts/Agent.gd
@@ -8,10 +8,14 @@ var velocity: Vector3 = Vector3.ZERO
 
 func _ready():
 	behaviour_algorithm = behaviour_algorithm.new(self)
-	behaviour_algorithm.on_enter()
+	if behaviour_algorithm.has_method("on_enter"):
+		behaviour_algorithm.on_enter()
 
 func _physics_process(delta):
-	behaviour_algorithm.on_update(delta)
+	if behaviour_algorithm.has_method("on_update"):
+		behaviour_algorithm.on_update(delta)
+	elif behaviour_algorithm.has_method("tick"):
+		behaviour_algorithm.tick()
 
 func move_towards(target, delta):
 	var move = Vector3.ZERO
@@ -23,3 +27,6 @@ func move_towards(target, delta):
 
 func is_near(target):
 	return target and translation.distance_to(target.translation) < 0.2
+
+func get_delta() -> float:
+	return get_physics_process_delta_time()
