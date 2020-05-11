caf296e8b9a25b1ec9beebc59fd575cb00441b3e
Jonathan Moallem <jdmoallem@gmail.com>
2020-05-11 16:12:25 +1000

    Made beacons display until attacked

--- scripts/Beacon.gd
+++ scripts/Beacon.gd
@@ -4,4 +4,9 @@ func _ready():
 	var _error = $Timer.connect("timeout", self, "toggle_signal")
 
 func toggle_signal():
-	visible = !visible
+	$Timer.stop()
+	visible = true
+
+func attack():
+	visible = false
+	$Timer.start()
