244fc95c72fa952867f22256d3da0a74aef2c1cb
Jonathan Moallem <jdmoallem@gmail.com>
2020-05-12 20:13:24 +1000

    Updated repeat filter for tick method and defaulting

--- scripts/BT/RepeatFilter.gd
+++ scripts/BT/RepeatFilter.gd
@@ -1,33 +1,27 @@
 class_name RepeatFilter
 extends Decorator
 
-var current_status
 var repeat_value
 var repeated = 0
 
 func _init(value: int, behaviour: Behaviour).(behaviour):
 	repeat_value = value
+	.on_initialise()
 
 func update() -> int:
 	if current_status == Status.SUCCESS:
 		return Status.SUCCESS
-	
-	var status = child.update()
+	var status = child.tick()
 	if status == Status.SUCCESS:
 		repeated += 1
-		child.on_terminate(Status.COMPLETED)
+		default()
 	elif status == Status.FAILURE:
 		return Status.FAILURE
 	else:
 		return status
 	
 	if repeated >= repeat_value:
-		current_status = Status.SUCCESS
+		repeated = 0
 		return Status.SUCCESS
 	else:
 		return Status.RUNNING
-
-func on_terminate(status) -> void:
-	repeated = 0
-	current_status = status
-	.on_terminate(status)
