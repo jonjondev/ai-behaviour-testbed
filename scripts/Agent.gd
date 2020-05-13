962383112dfa9e3f5ce1ece8ed07729cbc1c6de2
Jonathan Moallem <jdmoallem@gmail.com>
2020-05-13 19:20:44 +1000

    Added ability to slice AI time separately to physics processing

--- scripts/Agent.gd
+++ scripts/Agent.gd
@@ -7,6 +7,7 @@ var animation = AnimationManager.new(self)
 var blackboard = {}
 
 func _ready():
+	$AITime.connect("timeout", self , "ai_process")
 	behaviour_algorithm = behaviour_algorithm.new(self)
 	if behaviour_algorithm.has_method("on_enter"):
 		behaviour_algorithm.on_enter()
@@ -14,10 +15,11 @@ func _ready():
 func _physics_process(delta):
 	if behaviour_algorithm.has_method("on_update"):
 		behaviour_algorithm.on_update(delta)
-	elif behaviour_algorithm.has_method("tick"):
-		behaviour_algorithm.tick()
 	navigation.process()
 
+func ai_process():
+	if behaviour_algorithm.has_method("tick"):
+		behaviour_algorithm.tick()
 
 func is_near(target):
 	return target and translation.distance_to(target.translation) < 0.2
