8e1bb5081eeb7e23a3ad6c08b6d5034ee83b0955
Jonathan Moallem <jdmoallem@gmail.com>
2020-05-13 11:43:07 +1000

    Simplified behaviour tree repeat filter

--- scripts/BT/RepeatFilter.gd
+++ scripts/BT/RepeatFilter.gd
@@ -11,17 +11,17 @@ func _init(value: int, behaviour: Behaviour).(behaviour):
 func update() -> int:
 	if current_status == Status.SUCCESS:
 		return Status.SUCCESS
-	var status = child.tick()
-	if status == Status.SUCCESS:
-		repeated += 1
-		default()
-	elif status == Status.FAILURE:
-		return Status.FAILURE
-	else:
-		return status
-	
-	if repeated >= repeat_value:
-		repeated = 0
-		return Status.SUCCESS
-	else:
-		return Status.RUNNING
+	while true:
+		var status = child.tick()
+		match(status):
+			Status.RUNNING:
+				break
+			Status.FAILURE:
+				return Status.FAILURE
+			Status.SUCCESS:
+				repeated += 1
+				default()
+		if repeated >= repeat_value:
+			repeated = 0
+			return Status.SUCCESS
+	return Status.RUNNING
