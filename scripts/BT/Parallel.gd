bfce5e7f7cc1fdf954dfcebaa48337c800a5aafc
Jonathan Moallem <jdmoallem@gmail.com>
2020-05-14 21:43:51 +1000

    Updated parallels to allow success and failure policies to be specified

--- scripts/BT/Parallel.gd
+++ scripts/BT/Parallel.gd
@@ -1,8 +1,14 @@
 class_name Parallel
 extends Composite
 
-func _init(behaviours: Array).(behaviours):
-	pass
+enum Policy {REQ_ONE, REQ_ALL}
+
+var success_policy
+var failure_policy
+
+func _init(success: int, failure: int, behaviours: Array).(behaviours):
+	success_policy = success
+	failure_policy = failure
 
 func update() -> int:
 	var success_count = 0
@@ -13,11 +19,15 @@ func update() -> int:
 		match(status):
 			Status.SUCCESS:
 				success_count += 1
+				if success_policy == Policy.REQ_ONE:
+					return Status.SUCCESS
 			Status.FAILURE:
 				failure_count += 1
+				if failure_policy == Policy.REQ_ONE:
+					return Status.FAILURE
 		
-		if success_count >= children.size():
-			return Status.SUCCESS
-		elif failure_count > 0:
-			return Status.FAILURE
+	if failure_policy == Policy.REQ_ALL and failure_count >= children.size():
+		return Status.FAILURE
+	elif success_policy == Policy.REQ_ALL and success_count >= children.size():
+		return Status.SUCCESS
 	return Status.RUNNING
