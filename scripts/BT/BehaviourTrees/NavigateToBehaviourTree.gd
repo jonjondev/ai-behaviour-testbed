02f414f360c6f314086996adf96eaf4302b63a95
Jonathan Moallem <jdmoallem@gmail.com>
2020-05-14 21:48:51 +1000

    Updated navigate-to behaviour to use active selector instead of parallel

--- scripts/BT/BehaviourTrees/NavigateToBehaviourTree.gd
+++ scripts/BT/BehaviourTrees/NavigateToBehaviourTree.gd
@@ -3,7 +3,7 @@ extends BehaviourTree
 
 func _init(o, target_id).(o):
 	root = \
-	Parallel.new(Parallel.Policy.REQ_ONE, Parallel.Policy.REQ_ALL, [
+	ActiveSelector.new([
 		NearCondition.new(owner, target_id),
 		NavigateAction.new(owner, target_id),
 	])
