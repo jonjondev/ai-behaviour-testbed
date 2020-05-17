6d972681e5319fe9aa63056ec1305ba463c76a63
Jonathan Moallem <jdmoallem@gmail.com>
2020-05-17 17:50:34 +1000

    Added preconditions and effects to attack beacon GOAP action

--- scripts/GOAP/actions/AttackBeaconGoapAction.gd
+++ scripts/GOAP/actions/AttackBeaconGoapAction.gd
@@ -2,8 +2,19 @@ class_name AttackBeaconGoapAction
 extends GoapAction
 
 func _init(o).(o):
+	preconditions = {
+		GoapPlanner.StateConditions.BEACON_ALIVE: true,
+		GoapPlanner.StateConditions.WEAPON_ACQUIRED: true,
+	}
+	effects = {
+		GoapPlanner.StateConditions.BEACON_ALIVE: false,
+		GoapPlanner.StateConditions.WEAPON_ACQUIRED: false,
+	}
 	target = owner.get_tree().get_nodes_in_group("beacon")[0]
 
 func perform():
 	if owner.is_near(target):
 		target.attack()
+		owner.blackboard.weapon = false
+		return true
+	return false
