8b2a3043eb3d61988075a2912953258d0d1a51ca
Jonathan Moallem <jdmoallem@gmail.com>
2020-05-17 17:47:31 +1000

    Updated action state to avoid unnecessary navigation restarts

--- scripts/GOAP/states/ActionState.gd
+++ scripts/GOAP/states/ActionState.gd
@@ -2,19 +2,23 @@ class_name ActionState
 extends State
 
 var current_action
+var navigated = false
 
 func _init(owner).(owner):
 	pass
 
 func on_enter():
-	pass
+	current_action = owner.blackboard.plan.front() if owner.blackboard.plan else null
 
 func on_update():
-	current_action = owner.blackboard.plan.pop_front()
-	if current_action and current_action.target:
-		owner.blackboard.target = current_action.target
-	elif current_action:
-		current_action.perform()
+	if current_action:
+		if not navigated:
+			owner.blackboard.target = current_action.target
+			navigated = true
+		if current_action.perform():
+			current_action = owner.blackboard.plan.remove(0)
+			current_action = owner.blackboard.plan.front() if owner.blackboard.plan else null
+			navigated = false
 
 func on_exit():
 	pass
