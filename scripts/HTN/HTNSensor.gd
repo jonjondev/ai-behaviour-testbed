d1c129f37bf96876ff4ad8333ce949de9161c6ec
Jonathan Moallem <jdmoallem@gmail.com>
2020-05-21 23:09:54 +1000

    Created sensor class for HTN planner

---
+++ scripts/HTN/HTNSensor.gd
@@ -0,0 +1,29 @@
+class_name HTNSensor
+
+enum WorldStateProperties {
+	ENEMY_ALIVE,
+	ENEMY_RANGE,
+	WEAPON_AVAILABLE,
+	WEAPON_ACQUIRED,
+}
+
+enum EnemyRange {
+	MELEE_RANGE,
+	RANGED_RANGE,
+}
+
+var owner
+var blank_state = []
+
+func _init(o):
+	owner = o
+	for _i in range(WorldStateProperties.size()):
+		blank_state.append(null)
+
+func generate_current_state():
+	var current_state = blank_state.duplicate()
+	current_state[WorldStateProperties.ENEMY_ALIVE] = owner.get_tree().get_nodes_in_group("enemy")[0].visible
+	current_state[WorldStateProperties.ENEMY_RANGE] = EnemyRange.MELEE_RANGE if owner.distance_to(owner.get_tree().get_nodes_in_group("enemy")[0]) < 5 else EnemyRange.RANGED_RANGE
+	current_state[WorldStateProperties.WEAPON_AVAILABLE] = owner.get_tree().get_nodes_in_group("weapon")[0].visible
+	current_state[WorldStateProperties.WEAPON_ACQUIRED] = owner.blackboard.get("weapon") == true
+	return current_state
