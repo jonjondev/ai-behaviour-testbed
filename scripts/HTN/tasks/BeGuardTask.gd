72d9daf702513e86d0e19dfc9cdcc3aebfbc9aac
Jonathan Moallem <jdmoallem@gmail.com>
2020-05-22 18:39:29 +1000

    Finalised HTN domain task for guard

--- scripts/HTN/tasks/BeGuardTask.gd
+++ scripts/HTN/tasks/BeGuardTask.gd
@@ -1,29 +1,16 @@
 class_name BeGuardTask
 extends CompoundTask
 
-# CompoundTask[BeGuardTask]
-	# Method[ENEMY_ALIVE == true]
-		# Subtasks[AttackEnemyTask()]
-	# Method[true]
-		# Subtasks[NavigateToDoorTask(), NavigateToSafeTask()]
-# CompoundTask[AttackEnemyTask]
-	# Method[WEAPON_ACQUIRED == true]
-		# Subtasks[NavigateToEnemyTask(), DoEnemyAttackTask()]
-	# Method[true]
-		# Subtasks[NavigateToWeaponTask(), PickupWeaponTask(), AttackEnemyTask()]
-# PrimitiveTask[PickupWeaponTask] (weapon available)
-	# UseObjectOperator
-	# AnimateOperator
-		# Effects[WEAPON_AVAILABLE = false, WEAPON_ACQUIRED = true]
-# PrimitiveTask[DoEnemyAttackTask]
-	# UseObjectOperator
-	# AnimateOperator
-		# Effects[WEAPON_ACQUIRED = false, ENEMY_ALIVE = false]
-# PrimitiveTask[NavigateToEnemyTask]
-	# NavigateOperator
-# PrimitiveTask[NavigateToWeaponTask]
-	# NavigateOperator
-# PrimitiveTask[NavigateToDoorTask]
-	# NavigateOperator
-# PrimitiveTask[NavigateToSafeTask]
-	# NavigateOperator
+func _init():
+	method_tasks = [
+		[AttackEnemyTask.new()],
+		[NavigateToDoorTask.new(), NavigateToSafeTask.new()],
+	]
+
+func find_satisfied_method(working_worldstate: Array, cached_method: int) -> int:
+	if (cached_method != 0) and working_worldstate[HTNSensor.WorldStateProperties.ENEMY_ALIVE] == true:
+		return 0
+	elif (cached_method != 1):
+		return 1
+	else:
+		return -1
