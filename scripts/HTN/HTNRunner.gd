2e0e29622ceb341b73253e8576180f4cedabd4d2
Jonathan Moallem <jdmoallem@gmail.com>
2020-05-22 18:41:33 +1000

    Updated HTN runner to replan on world-state desynchronisation

--- scripts/HTN/HTNRunner.gd
+++ scripts/HTN/HTNRunner.gd
@@ -7,15 +7,25 @@ var planner = HTNPlanner.new()
 var current_plan = []
 var current_task = null
 
+var expected_worldstate
+
 func _init(o):
 	owner = o
 	sensor = sensor.new(o)
+	expected_worldstate = sensor.generate_current_state()
 
 func tick():
+	var real_worldstate = sensor.generate_current_state()
+	if expected_worldstate.hash() != real_worldstate.hash():
+		replan(real_worldstate)
 	if current_task:
 		if current_task.operate(owner):
+			expected_worldstate = planner.apply_effects(expected_worldstate, current_task.effects)
 			current_task = current_plan.pop_front()
 	elif current_plan.empty():
-		var working_worldstate = sensor.generate_current_state()
-		var current_plan = planner.generate_plan(working_worldstate)
-		current_task = current_plan.pop_front()
+		replan(real_worldstate)
+
+func replan(worldstate):
+	current_plan = planner.generate_plan(worldstate)
+	current_task = current_plan.pop_front()
+	expected_worldstate = worldstate
