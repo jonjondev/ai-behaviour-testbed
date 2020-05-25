2193c1bab8f3f26a72a2cbacfa67a764c65adb77
Jonathan Moallem <jdmoallem@gmail.com>
2020-05-25 15:18:32 +1000

    Added gun model and applied it to agent

--- scripts/AnimationManager.gd
+++ scripts/AnimationManager.gd
@@ -13,10 +13,13 @@ func start_anim(anim_name):
 			anim_running = true
 			agent.get_node("AlertedIndicator").visible = true
 			agent.get_tree().create_timer(1.0).connect("timeout", self, "stop_anim")
-		"armed":
-			agent.get_node("WeaponModel").visible = true
+		"armed_sword":
+			agent.get_node("SwordModel").visible = true
+		"armed_gun":
+			agent.get_node("GunModel").visible = true
 		"unarmed":
-			agent.get_node("WeaponModel").visible = false
+			agent.get_node("SwordModel").visible = false
+			agent.get_node("GunModel").visible = false
 
 func stop_anim():
 	agent.get_node("AlertedIndicator").visible = false
