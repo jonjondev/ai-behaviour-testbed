79a9b16221d6ae46a529f1d919b730eeee469f68
Jonathan Moallem <jdmoallem@gmail.com>
2020-05-25 16:06:34 +1000

    Made agent retreat when weapon are unavailable for HTN domain

--- scripts/HTN/tasks/PlanEnemyAttackTask.gd
+++ scripts/HTN/tasks/PlanEnemyAttackTask.gd
@@ -6,14 +6,17 @@ func _init():
 		[AttackEnemyTask.new()],
 		[NavigateToGunTask.new(), PickupGunTask.new(), self],
 		[NavigateToSwordTask.new(), PickupSwordTask.new(), self],
+		[RetreatTask.new()],
 	]
 
 func find_satisfied_method(working_worldstate: Array, cached_method: int) -> int:
-	if (cached_method != 0 and cached_method != 1 and cached_method != 2) and (working_worldstate[HTNSensor.WorldStateProperties.SWORD_ACQUIRED] == true or working_worldstate[HTNSensor.WorldStateProperties.GUN_ACQUIRED] == true):
+	if (cached_method != 0 and cached_method != 1 and cached_method != 2 and cached_method != 3) and (working_worldstate[HTNSensor.WorldStateProperties.SWORD_ACQUIRED] == true or working_worldstate[HTNSensor.WorldStateProperties.GUN_ACQUIRED] == true):
 		return 0
-	elif (cached_method != 1 and cached_method != 2):
+	elif (cached_method != 1 and cached_method != 2 and cached_method != 3):
 		return 1
-	elif (cached_method != 2):
+	elif (cached_method != 2 and cached_method != 3):
 		return 2
+	elif (cached_method != 3):
+		return 3
 	else:
 		return -1
